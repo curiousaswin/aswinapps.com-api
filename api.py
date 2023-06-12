8ce49b570b8b027448f8a0605a138028f82cecd7
curiousaswin <110875187+curiousaswin@users.noreply.github.com>
2023-06-12 17:58:25 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
 def get_data():
   """Returns the data in XML format."""
   data = {
-    "name": "John Doe",
-    "email": "johndoe@example.com",
-    "phone": "123-456-7890"
+    name:"Aswin's Good Apps",
+    website: "aswinapps.com",
+    publisher: "Aswin Arulvel",
+    latestapp: "AIstar",
+    latestappwebsite: "aistar.aswinapps.com",
+    about: "Aswin's Good Apps is a set of free apps that will always be free. Aswin's Good Apps. Free Apps for Real.",
+    slogan:"Free apps for real",
+    
   }
   return xml.etree.ElementTree.tostring(data, encoding="utf-8")
